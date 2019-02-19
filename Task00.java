4289d16d44b6cf7fb6ff74cbf7db5830f8a82e3f
hannayasko <47797802+hannayasko@users.noreply.github.com>
2019-02-20 01:32:45 +0300

    Add files via upload

---
+++ Task00.java
@@ -0,0 +1,9 @@
+public class Task00{
+	public static void main(String args[]) {
+		
+		Rectangle R = new Rectangle(1, 3);
+		System.out.println("Площадь прямоугольника со сторонами " + R.getSideA() + " и "+ R.getSideB() + " равна: " + R.getSquare());
+		System.out.println("Периметр  прямоугольника со сторонами " + R.getSideA() + " и "+ R.getSideB() + " равен: " + R.getPerimetr());
+		
+	}
+}
